15aff916654ff48aec7c76d6f0aeb937b9b567e7
gabbychuks <chukskel97@gmail.com>
2021-04-21 01:40:44 +0200

    First Commit

---
+++ mypython.py
@@ -0,0 +1,16 @@
+import numpy as np
+import random
+
+
+print('MY TEST PYTHON CODE....')
+for i in range(5):
+    print(i)
+print('\n\n')
+val = []
+for i in range(10):
+    x = random.randint(1, 20)
+    print(x)
+    val.append(x)
+    
+print(val)
+    
